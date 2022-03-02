b36e9ad75bee1338b34831fdfb88726b5080b419
chandu chandu <praneethas786@gmail.com>
2022-03-02 18:51:17 +0530

    user2: for loop

--- user.ts
+++ user.ts
@@ -1,8 +1,11 @@
-// hello user 2
+function loop(num:number){
+    for(let i=0;i<5;i++){
+        let c=num*i;
+        console.log(c)
+        if(i==4){
+            console.log("loop completed")
+        }
 
-function add(num1:number,num2:number){
-    let c=num1+num2;
-    console.log(c)
-
+        
+    }
 }
-add(10,60);
